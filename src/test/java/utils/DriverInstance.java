aab1ba5e5fe9cd1b26b4e4e3db5c0ac0ebbc75ef
Darwin Potes <39526465+djpotes@users.noreply.github.com>
2024-01-14 09:21:00 -0500

    Update DriverInstance.java

--- src/test/java/utils/DriverInstance.java
+++ src/test/java/utils/DriverInstance.java
@@ -16,20 +16,18 @@ public class DriverInstance {
      * Initialize the driver
      */
     public static void init(){
-        //if(driver == null){
-            String osName = System.getProperty("os.name").toLowerCase();
-            if(osName.contains("mac")){
-                System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-            }
-            if(osName.contains("win")){
-                System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-            }
-            System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
-            System.setProperty("webdriver.chrome.verboseLogging", "true");
-            ChromeOptions options = new ChromeOptions();
-            driver =  new ChromeDriver(options);
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
-        //}
+        String osName = System.getProperty("os.name").toLowerCase();
+        if(osName.contains("mac")){
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        }
+        if(osName.contains("win")){
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        }
+        System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
+        ChromeOptions options = new ChromeOptions();
+        driver =  new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
     }
     public static void quit(){
         driver.quit();
